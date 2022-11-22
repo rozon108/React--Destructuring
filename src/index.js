62b0b92da3e5cdff9679efb692baba0d762db73e
Rojan <rojan108p@gmail.com>
2022-11-22 15:28:02 +1100

    With solution from App Brewery

--- src/index.js
+++ src/index.js
@@ -29,3 +29,43 @@ ReactDOM.render(
   </table>,
   document.getElementById("root")
 );
+
+//Another Way
+// Looks clean and very interesting way of declaring const
+
+// const [honda, tesla] = cars;
+
+// const {
+//   speedStats: { topSpeed: hondaTopSpeed }
+// } = honda;
+// const {
+//   speedStats: { topSpeed: teslaTopSpeed }
+// } = tesla;
+
+// const {
+//   coloursByPopularity: [hondaTopColour]
+// } = honda;
+// const {
+//   coloursByPopularity: [teslaTopColour]
+// } = tesla;
+
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//       <th>Top Colour</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{teslaTopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{hondaTopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
